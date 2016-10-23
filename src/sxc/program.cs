cb7e86b0c461a48f271f991db75d377d33365b1f
Stuart Blackler (@Im5tu) <Im5tu@users.noreply.github.com>
2016-10-23 18:05:13 +0100

    Initial commit

---
+++ src/sxc/program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace sxc
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello sx lang");
+
+        }
+    }
+}
